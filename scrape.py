649ddc5ac38fcdd6caa200d1259abdec30f05afd
rocheandley kwidama <rocheandley@gmail.com>
2018-11-26 10:42:08 +0100

    1st commit

---
+++ scrape.py
@@ -0,0 +1,9 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "https://www.bloomberg.com/quote/SPX:IND%27"
+request = urllib.request.Request(url)
+response = urllib.request.urlopen(request)
+soup = BeautifulSoup(response, "html.parser")
+# print (response.read().decode('utf-8'))
+print(soup)
