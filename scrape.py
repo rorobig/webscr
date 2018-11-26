7ea3a93786be1e17b6828624ec029baf6c81426f
rocheandley kwidama <rocheandley@gmail.com>
2018-11-26 11:31:51 +0100

    added skeleton for basic scraping

--- scrape.py
+++ scrape.py
@@ -1,10 +1,19 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = "https://www.bloomberg.com/quote/SPX:IND%27"
+url = "https://www.pythonforbeginners.com"
 request = urllib.request.Request(url)
 response = urllib.request.urlopen(request)
 soup = BeautifulSoup(response, "html.parser")
 # print (response.read().decode('utf-8'))
-print(soup)
-print("hey")
+# print(soup)
+
+tags = soup.findAll("div", class_="post-bodycopy cf")
+# tags = soup.findAll("h2")
+
+print(tags[0].string)
+
+
+
+
+
